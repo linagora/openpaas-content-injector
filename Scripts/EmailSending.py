1d170180a63130bf861dc2684e0802f1450f78a2
Stéphane DROBOT <stephane.drobot@linagora.com>
2020-08-17 15:23:22 +0200

    #13 Add attachents to emails

--- Scripts/EmailSending.py
+++ Scripts/EmailSending.py
@@ -21,11 +21,12 @@ metaUrl =  urls['Email']['url']
 serverUrl = os.path.join(metaUrl, 'jmap')
 authUrl = os.path.join(metaUrl, 'authentication')
 uploadUrl = os.path.join(metaUrl, 'upload')
+downloadUrl = os.path.join(metaUrl, 'download')
 
 
 def getAccessToken(username: str, password: str, verbose=False) -> str:
+    "First request: get the authentication token"
 
-    #first request: asks for the token to be able to authentifiate
     headers= {"Accept": "application/json", "Content-Type": "application/json; charset=UTF-8"}
     body = {"username": username,
         "clientName": "Mozilla Firefox",
@@ -39,15 +40,14 @@ def getAccessToken(username: str, password: str, verbose=False) -> str:
     if verbose:
         print("Response of the first request : ", response)
         print("First json : ", firstJson)
-    
-    token = firstJson['continuationToken']
 
-    #second reqest: authentification
+    token = firstJson['continuationToken']
     bodySecond = {"token": token, "method": "password",    "password": password}
 
     response = s.post(authUrl, headers=headers, json = bodySecond)
 
     jsonSecond = response.json()
+
     if verbose:
         print("Response of the second request ", response)
         print("Second json : ", jsonSecond)
@@ -56,25 +56,27 @@ def getAccessToken(username: str, password: str, verbose=False) -> str:
 
 
 def setTokens(username: str, password: str) -> None:
+    "Set the global variables in order to use them for the next requests"
 
     global accessToken, headerAuth
     accessToken = getAccessToken(username, password)
     headerAuth = {"Accept": "application/json", "Content-Type": "application/json; charset=UTF-8", "Authorization": accessToken}
 
 
-def sendMail(sender: str,
-            senderName: str,
-            receivers: list,
-            cc:list =[],
-            subject='Mail subject',
-            message="Message to be sent",
-            outboxId='Error',
-            day=None, month=None, year=None, hour=10, minutes=0,
-            verbose= False
+def sendMail(sender : str,
+            senderName : str,
+            receivers : list,
+            cc:list = [],
+            subject = 'Mail subject',
+            message = "Message to be sent",
+            outboxId = 'Error',
+            attachments = [],
+            day = None, month = None, year = None, hour = 10, minutes = 0,
+            verbose = False
             ) -> (str, str):
 
     r"""Send an email from the sender adress the receivers, with copy for cc. Subject, body, date of the message... can be set
-    
+
     :param sender: email of the sender - must be the same as the one used for authentification
     :param senderName: name of the sender, as displayed in the email
     :param receivers: list of dict with email and name as keys
@@ -82,53 +84,54 @@ def sendMail(sender: str,
     :param subject: (optionnal) title of the email
     :params date and time of sending: (optionnal)"""
     
-    if day==None or month== None or year==None:
+    if day == None or month == None or year == None:
         d = datetime.datetime.now()
         date = d.replace(hour=d.hour-2).isoformat()+'Z'
     else:
-        date  = datetime.datetime(year, month, day, (hour-2)%24,minutes).isoformat()+'Z'
+        date = datetime.datetime(year, month, day, (hour-2)%24,minutes).isoformat()+'Z'
     
-    creationID = 'FillingOP'+ str(random.random())[2:]
+    creationID = 'FillingOP' + str(random.random())[2:]
     bodySending = [["setMessages",{
-        "create":{
-            creationID:{
-                "bcc":[],
-                "cc":cc,
-                "from":{
-                    "email":sender,
-                    "name":senderName
+        "create" : {
+            creationID : {
+                "bcc" : [],
+                "cc" : cc,
+                "from" : {
+                    "email" : sender,
+                    "name" : senderName
                 },
-                "mailboxIds":[outboxId],
-                "htmlBody":message,
-                "replyTo":[{
-                    "email":sender,
-                    "name":senderName
+                "mailboxIds" : [outboxId],
+                "htmlBody" : message,
+                "replyTo" : [{
+                    "email" : sender,
+                    "name" : senderName
                 }],
-                "subject":subject,
-                "to":receivers,
-                "date": date
+                "subject" : subject,
+                "to" : receivers,
+                "date" : date,
+                "attachments" : attachments
             }
         }
     },
     "#0"]]
 
-    response = s.post(serverUrl, headers=headerAuth, json= bodySending, verify=False)
+    response = s.post(serverUrl, headers= headerAuth, json= bodySending, verify= False)
     mailJson  = response.json()
     
     mailId = mailJson[0][1]['created'][creationID]['id']
+
     if verbose:
         print(mailJson[0][1]['created'][creationID])
+
     return mailId, creationID
 
 
 def getOutboxID() -> str:
-
     "Get the ID ouf the outbox folder, needed to send emails for ex."
+
     bodyInboxList = [["getMailboxes",{},"#0"]]
 
-
     response = s.post(serverUrl, headers= headerAuth, json= bodyInboxList, verify=False)
-
     boxJson = response.json()
 
     outboxID = None
@@ -139,16 +142,48 @@ def getOutboxID() -> str:
     return 'Error : No outbox found'
     
 
-def uploadFile(filePath:str) -> None:
-    
-    response = s.post(uploadUrl, headers= headerAuth, verify=False)
-    #TO DO
+def uploadFile(filePath:str) -> dict:
+    "Upload the file from the path and return the dictionnary to add to the attachments"
+
+    headerUpload = headerAuth
+    contentType = os.path.splitext(filePath)[1]
+
+    if contentType in ['.png','.jpeg', '.gif']:
+        headerUpload["Content-Type"] = 'image/'+ contentType[1:]
+
+    elif contentType in ['.pdf', '.json', '.xml']:
+        headerUpload["Content-Type"] = 'application/'+ contentType[1:]
+
+    elif contentType == '.odt':
+        headerUpload["Content-Type"] = 'application/vnd.oasis.opendocument.text'
+
+    elif contentType == '.ods':
+        headerUpload["Content-Type"] = 'application/vnd.oasis.opendocument.spreadsheet'
+
+    elif contentType == '.odp':
+        headerUpload["Content-Type"] = 'application/vnd.oasis.opendocument.presentation'
+
+
+    response = s.post(uploadUrl, headers= headerUpload, verify=False, data= filePath)
+    uploadJson = response.json()
+    fileName = os.path.basename(filePath)
+
+    attachments = {"blobId" : uploadJson['blobId'],
+                    "type" : uploadJson['type'],
+                    "name" : fileName,
+                    "size": uploadJson['size'],
+                    "url": os.path.join(downloadUrl, uploadJson['blobId'],fileName),
+                    "isInline" : False}
+
+    return attachments
+
 
 def calculDate(sendingDate:datetime.date, weekday:str, hours:str):
-    conversion = {'mo': 0, 'tu': 1, 'we':2, 'th':3, 'fr':4, 'to':15}
+
+    conversion = {'mo' : 0, 'tu' : 1, 'we' : 2, 'th' : 3, 'fr' : 4, 'to' : 15}
     hourb, houre = hours.split('-')
-    hourb = hourb[:2]+ ':00 '+ hourb[2:].upper()
-    houre = houre[:2]+ ':00 '+ houre[2:].upper()
+    hourb = hourb[:2] + ':00 ' + hourb[2:].upper()
+    houre = houre[:2] + ':00 ' + houre[2:].upper()
     if weekday == 'to':
         return sendingDate, hourb, houre
     else:
@@ -158,15 +193,15 @@ def calculDate(sendingDate:datetime.date, weekday:str, hours:str):
                 return varDate, hourb, houre
 
 
-
-def main(language:str, month:str, day:str, year:str, mailADay:str = 2) -> list(dict()):
+def main(language:str, month:str, day:str, year:str, mailADay:int = 2) -> list(dict()):
     "Send an (optionnaly) given number of mails for each days beetween the given date and the end of the month"
-    
+
     cred = configparser.ConfigParser()
     cred.read(os.path.join(dataPath, 'Config', 'logins'))
     logins = cred.sections()
 
     listMessages = os.listdir(os.path.join(dataPath, 'Mails', language))
+    listAttachments = os.listdir(os.path.join(dataPath, 'Mails', 'Attachments', language))
     d = int(day)
     m = int(month)
     y = int(year)
@@ -185,74 +220,73 @@ def main(language:str, month:str, day:str, year:str, mailADay:str = 2) -> list(d
                 message = messageFile.read()
                 messageFile.close()
                 subject, *attribute = messageName.split('_')
-                #print(messageName)
 
+                
                 #Select one or more email adresses
                 #Personnalize the mail
+
+                cc = []
+                attachments = []
                 if 'copy' in attribute:
                     sender, rec, cop = random.sample(range(len(logins)), 3)
-                    send = cred[logins[sender]]
-                    mail, passw, name, lastname = send['login'], send['password'], send['first_name'], send['last_name']
                     receiver = cred[logins[rec]]
                     copy = cred[logins[cop]]
-                    realMessage = message.format(receiver=receiver['first_name'], sender=name +' '+ lastname, senderMail=mail, in_copy=copy['first_name'])
-                    receivers=[{'email': receiver['login'], 'name': logins[rec]}]
-                    cc= [{'email': copy['login'], 'name': logins[cop]}]
+                    realMessage = message.format(receiver = receiver['first_name'], sender = logins[sender],
+                                                senderMail = cred[logins[sender]]['login'], in_copy = copy['first_name'])
+                    receivers = [{'email' : receiver['login'], 'name' : logins[rec]}]
+                    cc = [{'email' : copy['login'], 'name' : logins[cop]}]
                 
                 elif 'many' in attribute:
                     sender, *rec = random.sample(range(len(logins)), len(logins))
-                    send = cred[logins[sender]]
-                    mail, passw, name, lastname = send['login'], send['password'], send['first_name'], send['last_name']
                     receivers = []
+                    realMessage = message.format(receiver = receiver['first_name'], sender = logins[sender],
+                                                senderMail = cred[logins[sender]]['login'])
                     for j in rec:
                         temp = cred[logins[j]]
-                        receivers.append({'email': temp['login'], 'name': logins[j]})
-                    cc= []
+                        receivers.append({'email' : temp['login'], 'name' : logins[j]})
+                    print(logins[sender], ' to ', str([logins[i] for i in rec]))
 
                 elif 'attach' in attribute:
                     sender, rec = random.sample(range(len(logins)), 2)
-                    send = cred[logins[sender]]
-                    mail, passw, name, lastname = send['login'], send['password'], send['first_name'], send['last_name']
                     receiver = cred[logins[rec]]
-                    realMessage = message.format(receiver=receiver['first_name'], sender=name+' '+ lastname, senderMail=mail)
-                    receivers=[{'email': receiver['login'], 'name': logins[rec]}]
-                    cc= []
+                    realMessage = message.format(receiver = receiver['first_name'], sender = logins[sender], senderMail = cred[logins[sender]]['login'])
+                    receivers = [{'email' : receiver['login'], 'name' : logins[rec]}]
+                    fileName = listAttachments[random.randrange(len(listAttachments))]
+                    filePath = os.path.join(dataPath, 'Mails','Attachments', language, fileName)
+                    attachments.append(uploadFile(filePath))
 
                 elif 'event' in attribute:
                     sender, rec = random.sample(range(len(logins)), 2)
-                    send = cred[logins[sender]]
-                    mail, passw, name, lastname = send['login'], send['password'], send['first_name'], send['last_name']
                     receiver = cred[logins[rec]]
                     
                     _, eventName, weekday, hours = attribute
                     eventDate, hour1, hour2 = calculDate(sendingDate, weekday, hours)
-
-                    realMessage = message.format(receiver=receiver['first_name'], sender=name +' '+ lastname, senderMail=mail, eventDate= eventDate.strftime("%A %d."))
-                    receivers=[{'email': receiver['login'], 'name': logins[rec]}]
-                    eventList.append({'date': eventDate.strftime("%a %Y/%m/%d"), 'eventName': eventName, 
-                                    'begHour': hour1, 'endHour': hour2, 'organizer':mail})
-                    cc= []
+                    realMessage = message.format(receiver = receiver['first_name'], sender = logins[sender],
+                                                senderMail = cred[logins[sender]]['login'], eventDate = eventDate.strftime("%A %d."))
+                    receivers = [{'email' : receiver['login'], 'name' : logins[rec]}]
+                    eventList.append({'date' : eventDate.strftime("%a %Y/%m/%d"), 'eventName' : eventName,
+                                    'begHour' : hour1, 'endHour' : hour2, 'organizer' : mail})
                     
                 else:
                     sender, rec = random.sample(range(len(logins)), 2)
-                    send = cred[logins[sender]]
-                    mail, passw, name, lastname = send['login'], send['password'], send['first_name'], send['last_name']
                     receiver = cred[logins[rec]]
-                    realMessage = message.format(receiver=receiver['first_name'], sender=name+' '+ lastname, senderMail=mail)
-                    receivers=[{'email': receiver['login'], 'name': logins[rec]}]
-                    cc= []
+                    realMessage = message.format(receiver = receiver['first_name'], sender = logins[sender], senderMail = cred[logins[sender]]['login'])
+                    receivers=[{'email' : receiver['login'], 'name' : logins[rec]}]
 
                 #Set tokens for the corresponding login
+                send = cred[logins[sender]]
+                mail, passw = send['login'], send['password']
                 setTokens(mail, passw)
                 
-                print(name +' '+ lastname, ' to ', logins[rec])
+                if not 'many' in attribute:
+                    print(logins[sender], ' to ', logins[rec])
 
                 outboxId = getOutboxID()
 
-                a,b = sendMail(sender=mail, senderName=name+' '+ lastname, subject=subject, message=realMessage, 
-                        receivers=receivers, cc=cc,
-                        outboxId=outboxId, 
-                        year=sendingDate.year, day=sendingDate.day, month=sendingDate.month, hour= random.randint(9,20), minutes=random.randint(0,59) )
+                a,b = sendMail(sender = mail, senderName = logins[sender], subject = subject, message = realMessage,
+                        receivers = receivers, cc = cc,
+                        outboxId = outboxId,
+                        year = sendingDate.year, day = sendingDate.day, month = sendingDate.month, hour = random.randint(9,20), minutes = random.randint(0,59) )
 
                         
     return eventList
